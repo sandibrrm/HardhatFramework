a05a25774139c29db630755d51a5e7f9701fa3f8
Sandi <43517564+sandibrrm@users.noreply.github.com>
2025-08-07 03:42:22 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/hardhatframework/main.go
@@ -0,0 +1,23 @@
+// cmd/hardhatframework/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "hardhatframework/internal/hardhatframework"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := hardhatframework.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
